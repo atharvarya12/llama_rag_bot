c9d2ad89c9786b25f28769ea13bb4939b35d888b
Atharv Arya <47898165+atharvarya12@users.noreply.github.com>
2024-05-19 12:16:56 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,48 @@
+
+import os
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.memory import ConversationSummaryMemory
+from langchain_openai import OpenAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.llms import Ollama
+
+
+data_path = input("give the text book location: ")
+
+
+
+embedding_func = OpenAIEmbeddings(openai_api_key = "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")
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=2000,
+    chunk_overlap=30,
+    length_function=len,)
+
+documents = PyPDFLoader(data_path).load_and_split(text_splitter=text_splitter)
+
+
+
+vectordb = Chroma.from_documents(documents, embedding=embedding_func)
+
+
+
+template = '''[INST]given the context - {context} [INST] [INST] Answer the following question {question}[/INST] '''
+
+pt = PromptTemplate(
+            template=template, input_variables=["context", "question"]
+        )
+     
+
+rag = RetrievalQA.from_chain_type(
+            llm=Ollama(model="phi"),
+            retriever=vectordb.as_retriever(),
+            memory=ConversationSummaryMemory(llm = Ollama(model="phi")),
+            chain_type_kwargs={"prompt": pt, "verbose": True},
+        )
+
+rag.invoke(input("ask me anything: "))
+
